2a70b54554654f696d07bec26e361063c0a0aca8
doommot <34127346+doommot@users.noreply.github.com>
2018-04-07 13:10:44 +0300

    100accs v0.1
    temporary blocked rate raising

--- SMSreg.py
+++ SMSreg.py
@@ -9,140 +9,141 @@ import math
 
 #TODO: log every answer
 class SMSreg:
-        currentRate = 3.0
-        
-        #tzid=None
-        '''
-        def __init__(SMSreg):
-                r = requests.get('http://api.sms-reg.com/getNum.php?country=all&service=telegram&appid='+APIkey)
-                if (not r.json()['responce']=='1'):
-                        raise r.json()['responce']
-                SMSreg.tzid=r.json()['tzid']
-                __log('number received')
-        '''
+	currentRate = 3.0
+	
+	#tzid=None
+	'''
+	def __init__(SMSreg):
+		r = requests.get('http://api.sms-reg.com/getNum.php?country=all&service=telegram&appid='+APIkey)
+		if (not r.json()['responce']=='1'):
+			raise r.json()['responce']
+		SMSreg.tzid=r.json()['tzid']
+		__log('number received')
+	'''
 
-        def __setRate(value):
-                SMSreg.__log('Setting rate (__setRate func), value = ' + str(value))
-                
-                r = requests.get('http://api.sms-reg.com/setRate.php?rate=' + str(float(value)) + '&apikey=' + config.APIkey).json()
-                
-                if r['response'] != '1' or not math.isclose(float(r['rate']), float(value)):
-                        SMSreg.__log("Some error occured, r['response'] = " + r['response'])
-                        try:
-                                SMSreg.__log("r['rate'] = " + str(r['rate']))
-                        except KeyError:
-                                pass
+	def __setRate(value):
+		SMSreg.__log('Setting rate (__setRate func), value = ' + str(value))
+		
+		r = requests.get('http://api.sms-reg.com/setRate.php?rate=' + str(float(value)) + '&apikey=' + config.APIkey).json()
+		
+		if r['response'] != '1' or not math.isclose(float(r['rate']), float(value)):
+			SMSreg.__log("Some error occured, r['response'] = " + r['response'])
+			try:
+				SMSreg.__log("r['rate'] = " + str(r['rate']))
+			except KeyError:
+				pass
+		else:
+			SMSreg.__log('Successfully set rate')
+			
+			#SMSreg.__setRate(value)
 
-                        
-                        SMSreg.__setRate(value)
+		SMSreg.__log('finish setting rate')#('Successfully set rate')
 
-                SMSreg.__log('Successfully set rate')
+	def raiseRate(value):
+		SMSreg.currentRate += value
+		SMSreg.currentRate = min(SMSreg.currentRate, config.maxRate)
+		
+		SMSreg.__setRate(SMSreg.currentRate)
+	
+	def getNum():#returns number
+		SMSreg.__setRate(SMSreg.currentRate)
+	
+		r = requests.get('http://api.sms-reg.com/getNum.php?country=all&service=telegram&appid='+config.APP_APIKEY+'&apikey='+config.APIkey)
+		if (r.json()['response']=='ERROR'):
+			if (r.json()['error_msg']=='ERROR_WRONG_KEY'):
+				pass
 
-        def raiseRate(value):
-                SMSreg.currentRate += value
-                SMSreg.currentRate = min(SMSreg.currentRate, config.maxRate)
-                
-                SMSreg.__setRate(SMSreg.currentRate)
-        
-        def getNum():#returns number
-                SMSreg.__setRate(SMSreg.currentRate)
-        
-                r = requests.get('http://api.sms-reg.com/getNum.php?country=all&service=telegram&appid='+config.APP_APIKEY+'&apikey='+config.APIkey)
-                if (r.json()['response']=='ERROR'):
-                        if (r.json()['error_msg']=='ERROR_WRONG_KEY'):
-                                pass
+		SMSreg.__log('Operation begun, tzid is '+r.json()['tzid'])
+		#return r.json()['tzid']
 
-                SMSreg.__log('Operation begun, tzid is '+r.json()['tzid'])
-                #return r.json()['tzid']
+		for i in range (0,60):
+			time.sleep(2)
+			SMSreg.__log('getting state')
+			Num=requests.get('http://api.sms-reg.com/getState.php?tzid='+r.json()['tzid']+'&apikey='+config.APIkey)
+			if (Num.json()['response']=='TZ_NUM_PREPARE'):
+				SMSreg.__log('JSON response is "TZ_NUM_PREPARE"')
+				SMSreg.__log('phone is '+Num.json()['number'])
 
-                for i in range (0,60):
-                        time.sleep(2)
-                        SMSreg.__log('getting state')
-                        Num=requests.get('http://api.sms-reg.com/getState.php?tzid='+r.json()['tzid']+'&apikey='+config.APIkey)
-                        if (Num.json()['response']=='TZ_NUM_PREPARE'):
-                                SMSreg.__log('JSON response is "TZ_NUM_PREPARE"')
-                                SMSreg.__log('phone is '+Num.json()['number'])
+				SMSreg.currentRate -= config.lowerRateOnSuccess
+				if SMSreg.currentRate < 2.0:
+					SMSreg.currentRate = 2.0
 
-                                SMSreg.currentRate -= config.lowerRateOnSuccess
-                                if SMSreg.currentRate < 2.0:
-                                        SMSreg.currentRate = 2.0
+				SMSreg.__log('Set VARIABLE SMSreg.currentRate to be ' + str(SMSreg.currentRate))
+				
+				return {'num' : Num.json()['number'], 'tzid' : r.json()['tzid']}
+			elif (Num.json()['response']=='TZ_INPOOL'):
+				SMSreg.__log('waiting for number')
+			else:
+				SMSreg.__log('Operation status is not valid. Trying again (status == ' + Num.json()['response'] + ')')
+				
+				if (Num.json()['response'] == 'WARNING_NO_NUMS'):
+					SMSreg.raiseRate(config.raiseRateOnFail)
+				
+				return SMSreg.getNum()
 
-                                SMSreg.__log('Set VARIABLE SMSreg.currentRate to be ' + str(SMSreg.currentRate))
-                                
-                                return {'num' : Num.json()['number'], 'tzid' : r.json()['tzid']}
-                        elif (Num.json()['response']=='TZ_INPOOL'):
-                                SMSreg.__log('waiting for number')
-                        else:
-                                SMSreg.__log('Operation status is not valid. Trying again (status == ' + Num.json()['response'] + ')')
-                                
-                                if (Num.json()['response'] == 'WARNING_NO_NUMS'):
-                                        SMSreg.raiseRate(config.raiseRateOnFail)
-                                
-                                return SMSreg.getNum()
+	def wrongNum(tzid):
+		time.sleep(15)#this method should be used after 15sec min since getNum
+		while((requests.get('http://api.sms-reg.com/setOperationUsed.php?tzid='+tzid+'&apikey='+config.APIkey)).json()['response'] != '1'):
+			SMSreg.__log('setOperationUsed did not work properly')
+			time.sleep(5)
 
-        def wrongNum(tzid):
-                time.sleep(15)#this method should be used after 15sec min since getNum
-                while((requests.get('http://api.sms-reg.com/setOperationUsed.php?tzid='+tzid+'&apikey='+config.APIkey)).json()['response'] != '1'):
-                        SMSreg.__log('setOperationUsed did not work properly')
-                        time.sleep(5)
+		while((requests.get('http://api.sms-reg.com/setOperationOk.php?tzid='+tzid+'&apikey='+config.APIkey)).json()['response'] != '1'):
+			SMSreg.__log('setOperationOk did not work properly')
+			time.sleep(5)
 
-                while((requests.get('http://api.sms-reg.com/setOperationOk.php?tzid='+tzid+'&apikey='+config.APIkey)).json()['response'] != '1'):
-                        SMSreg.__log('setOperationOk did not work properly')
-                        time.sleep(5)
+	def getCode(tzid):#returns code from sms
+		r = requests.get('http://api.sms-reg.com/setReady.php?tzid='+tzid+'&apikey='+config.APIkey)
+		if (not r.json()['response']=='1'):
+			SMSreg.__log(r.json()['response'])
+			raise Exception(r.json()['response'])
 
-        def getCode(tzid):#returns code from sms
-                r = requests.get('http://api.sms-reg.com/setReady.php?tzid='+tzid+'&apikey='+config.APIkey)
-                if (not r.json()['response']=='1'):
-                        SMSreg.__log(r.json()['response'])
-                        raise Exception(r.json()['response'])
+		
+		for i in range (0, 50):
+			r = requests.get('http://api.sms-reg.com/getState.php?tzid='+tzid+'&apikey='+config.APIkey)
+			SMSreg.__log('Waiting for code to come')
+			time.sleep(3)
+			if ((r.json()['response']=='TZ_NUM_ANSWER') or (r.json()['response'])=='TZ_NUM_ANSWER2'):
+				return r.json()['msg']
+			if (not ((r.json()['response']=='TZ_NUM_WAIT') or (r.json()['response'])=='TZ_NUM_WAIT2')):
+				SMSreg.__log(r.json()['response'])
+				raise Exception(r.json()['response'])
 
-                
-                for i in range (0, 50):
-                        r = requests.get('http://api.sms-reg.com/getState.php?tzid='+tzid+'&apikey='+config.APIkey)
-                        SMSreg.__log('Waiting for code to come')
-                        time.sleep(3)
-                        if ((r.json()['response']=='TZ_NUM_ANSWER') or (r.json()['response'])=='TZ_NUM_ANSWER2'):
-                                return r.json()['msg']
-                        if (not ((r.json()['response']=='TZ_NUM_WAIT') or (r.json()['response'])=='TZ_NUM_WAIT2')):
-                                SMSreg.__log(r.json()['response'])
-                                raise Exception(r.json()['response'])
+		return 0
 
-                return 0
+	def reviseCode(tzid):#returns new code
+		r = requests.get('http://api.sms-reg.com/setOperationRevise.php?tzid='+tzid+'&apikey='+config.APIkey)
+		if (not r.json()['responce']=='1'):
+			SMSreg.__log(r.json()['response'])
+			raise Exception(r.json()['responce'])
+		for i in range (0,20):
+			time.sleep(3)
+			r = requests.get('http://api.sms-reg.com/getState.php?tzid='+tzid+'&apikey='+config.APIkey)
+			if ((r.json()['responce']=='TZ_NUM_ANSWER') or (r.json()['responce'])=='TZ_NUM_ANSWER2'):
+				return r.json()['msg']
+			if (not ((r.json()['response']=='TZ_NUM_WAIT') or (r.json()['responce'])=='TZ_NUM_WAIT2')):
+				SMSreg.__log(r.json()['response'])
+				raise Exception(r.json()['response'])
 
-        def reviseCode(tzid):#returns new code
-                r = requests.get('http://api.sms-reg.com/setOperationRevise.php?tzid='+tzid+'&apikey='+config.APIkey)
-                if (not r.json()['responce']=='1'):
-                        SMSreg.__log(r.json()['response'])
-                        raise Exception(r.json()['responce'])
-                for i in range (0,20):
-                        time.sleep(3)
-                        r = requests.get('http://api.sms-reg.com/getState.php?tzid='+tzid+'&apikey='+config.APIkey)
-                        if ((r.json()['responce']=='TZ_NUM_ANSWER') or (r.json()['responce'])=='TZ_NUM_ANSWER2'):
-                                return r.json()['msg']
-                        if (not ((r.json()['response']=='TZ_NUM_WAIT') or (r.json()['responce'])=='TZ_NUM_WAIT2')):
-                                SMSreg.__log(r.json()['response'])
-                                raise Exception(r.json()['response'])
+	def wrongCode(tzid):
+		r = requests.get('http://api.sms-reg.com/setOperationOver.php?tzid='+tzid+'&apikey='+config.APIkey)
+		if (not r.json()['response']=='1'):
+			SMSreg.__log(r.json()['response'])
+			raise Exception(r.json()['response'])
 
-        def wrongCode(tzid):
-                r = requests.get('http://api.sms-reg.com/setOperationOver.php?tzid='+tzid+'&apikey='+config.APIkey)
-                if (not r.json()['response']=='1'):
-                        SMSreg.__log(r.json()['response'])
-                        raise Exception(r.json()['response'])
+	def getBalance():
+		r = requests.get('http://api.sms-reg.com/getBalance.php?apikey='+config.APIkey)
+		if (not r.json()['response']=='1'):
+			SMSreg.__log(r.json()['response'])
+			raise Exception(r.json()['response'])
+		return r.json()['balance']
 
-        def getBalance():
-                r = requests.get('http://api.sms-reg.com/getBalance.php?apikey='+config.APIkey)
-                if (not r.json()['response']=='1'):
-                        SMSreg.__log(r.json()['response'])
-                        raise Exception(r.json()['response'])
-                return r.json()['balance']
+	def finish(tzid):
+		r = requests.get('http://api.sms-reg.com/setOperationOk.php?tzid='+tzid + '&apikey=' + config.APIkey)
+		if (not r.json()['response']=='1'):
+			SMSreg.__log('On finish response = ' + r.json()['response'])
 
-        def finish(tzid):
-                r = requests.get('http://api.sms-reg.com/setOperationOk.php?tzid='+tzid + '&apikey=' + config.APIkey)
-                if (not r.json()['response']=='1'):
-                        SMSreg.__log('On finish response = ' + r.json()['response'])
-
-        def __log(line):
-                log_stream = open(config.logfile, "a", encoding = 'utf8')
-                mem = 'SREG (' + ') [' + datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S;") + ']: ' + line + '\n'
-                log_stream.write(mem)
-                log_stream.close()
+	def __log(line):
+		log_stream = open(config.logfile, "a", encoding = 'utf8')
+		mem = 'SREG (' + ') [' + datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S;") + ']: ' + line + '\n'
+		log_stream.write(mem)
+		log_stream.close()
--- account.py
+++ account.py
@@ -12,140 +12,140 @@ from telethon.tl.functions.account import UpdateStatusRequest
 
 class Account:
 #variables
-        client = None
-        phone = None
+	client = None
+	phone = None
 
-        proxyName = None
-        proxyPort = None
-        
+	proxyName = None
+	proxyPort = None
+	
 #private funcs:
-        def log(self, string):
-                log_stream = open(config.logfile, "a", encoding = 'utf8')
-                mem = 'ACCN (' + self.phone + ') [' + datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S;") + ']: ' + string + '\n'
-                log_stream.write(mem)
-                log_stream.close()
-        
-        
-        def __init__(self, phone = None):
-                '''
-                Конструктор создает клиент и гарантирует соединение с сервером в случае успешного завершения.
+	def log(self, string):
+		log_stream = open(config.logfile, "a", encoding = 'utf8')
+		mem = 'ACCN (' + self.phone + ') [' + datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S;") + ']: ' + string + '\n'
+		log_stream.write(mem)
+		log_stream.close()
+	
+	
+	def __init__(self, phone = None):
+		'''
+		Конструктор создает клиент и гарантирует соединение с сервером в случае успешного завершения.
 
-                Параметр phone - номер, привязаный к этому аккаунту в последствии сменить НЕЛЬЗЯ
+		Параметр phone - номер, привязаный к этому аккаунту в последствии сменить НЕЛЬЗЯ
 
-                Если phone не указан, то происходит регистрация нового аккаунта, phone берется из сервиса активаций.
-                '''
+		Если phone не указан, то происходит регистрация нового аккаунта, phone берется из сервиса активаций.
+		'''
 
-                #self.proxyName = prox_name
-                #self.proxyPort = prox_port
-                
-                if(phone):
-                        self.phone = phone
-                        self.__connect()
-                else:
-                        self.__reg()
-
-
-        def __connect(self):
-                try:
-                        self.client = TelegramClient(config.dataSavePath + self.phone, config.api_id, config.api_hash)#, proxy = (socks.SOCKS5, self.proxyName, self.proxyPort))
-                        for i in range(50):
-                                self.log("Connecting...")
-                                
-                                if self.client.connect():
-                                        self.log("Successfully connected to the server")
-                                        return
-                        
-                        self.log("UNABLE TO CONNECT TO THE SERVER")
-                        return False
-                           
-                except PermissionError:
-                        self.log("Unable to create account!!! An error appeared due to wrong api_id or api_hash")
-                        return False
-
-                except ValueError:
-                        self.log("Unable to create account!!! An error appeared due to wrong Session parameter")
-                        return False
-
-                
-        
-        def __reg(self):
-                numResponse = SMSreg.getNum()
-
-                self.phone = numResponse['num']
-
-                self.log("Phone got. Registration is starting, self.phone="+self.phone)
-                try:
-                        self.__connect()
-                        self.log("Sending code request for this account")
-                        self.client.send_code_request(self.phone)
-
-                except telethon.errors.rpc_error_list.PhoneNumberBannedError:
-                        self.log('This phone number is banned')
-
-                        SMSreg.wrongNum(numResponse['tzid'])
-                        self.__reg()
-                
-                '''
-                except PhoneNumberInvalidError:
-                        self.log('phone number invalid')
-                        self.__init__(proxyName, proxyPort)
-                '''
-                '''
-                except PhoneNumberBannedError:
-                        self.log('phone number banned')
-                        SMSreg.wrongNum(self.phone)
+		#self.proxyName = prox_name
+		#self.proxyPort = prox_port
+		
+		if(phone):
+			self.phone = phone
+			self.__connect()
+		else:
+			self.__reg()
 
 
-                '''
+	def __connect(self):
+		try:
+			self.client = TelegramClient(config.dataSavePath + self.phone, config.api_id, config.api_hash)#, proxy = (socks.SOCKS5, self.proxyName, self.proxyPort))
+			for i in range(50):
+				self.log("Connecting...")
+				
+				if self.client.connect():
+					self.log("Successfully connected to the server")
+					return
+			
+			self.log("UNABLE TO CONNECT TO THE SERVER")
+			return False
+			   
+		except PermissionError:
+			self.log("Unable to create account!!! An error appeared due to wrong api_id or api_hash")
+			return False
 
-                fname = random.choice(names.firstnames)
-                lname = random.choice(names.lastnames)
-                self.log('The chosen name for acc is ' + fname + ' ' + lname)
-                Code=SMSreg.getCode(numResponse['tzid'])
+		except ValueError:
+			self.log("Unable to create account!!! An error appeared due to wrong Session parameter")
+			return False
 
-                if Code == 0:
-                        self.client.send_code_request(self.phone, True)
-                        Code = SMSreg.getCode(numResponse['tzid'])
-                
-                try:
-                        self.client.sign_up(Code, fname, lname)
+		
+	
+	def __reg(self):
+		numResponse = SMSreg.getNum()
 
-                except telethon.errors.rpc_error_list.PhoneNumberOccupiedError:
-                        self.log('This phone number is already occupied')
-                        try:
-                                self.client.sign_in(phone=self.phone, code=Code)
-                        except telethon.errors.rpc_error_list.SessionPasswordNeededError:
-                                SMSreg.finish(numResponse['tzid'])
-                                self.__reg()
-                
-                '''
-                except Exception:
-                        try:
-                                self.client.sign_up(SMSreg.reviseCode(self.phone), fname, lname)
-                        except Exception:
-                                SMSreg.wrongCode(self.phone)
-                '''
-                SMSreg.finish(numResponse['tzid'])
-                self.log("Client is signed up")
+		self.phone = numResponse['num']
+
+		self.log("Phone got. Registration is starting, self.phone="+self.phone)
+		try:
+			self.__connect()
+			self.log("Sending code request for this account")
+			self.client.send_code_request(self.phone)
+
+		except telethon.errors.rpc_error_list.PhoneNumberBannedError:
+			self.log('This phone number is banned')
+
+			SMSreg.wrongNum(numResponse['tzid'])
+			self.__reg()
+		
+		'''
+		except PhoneNumberInvalidError:
+			self.log('phone number invalid')
+			self.__init__(proxyName, proxyPort)
+		'''
+		'''
+		except PhoneNumberBannedError:
+			self.log('phone number banned')
+			SMSreg.wrongNum(self.phone)
+
+
+		'''
+
+		fname = random.choice(names.firstnames)
+		lname = random.choice(names.lastnames)
+		self.log('The chosen name for acc is ' + fname + ' ' + lname)
+		Code=SMSreg.getCode(numResponse['tzid'])
+
+		if Code == 0:
+			self.client.send_code_request(self.phone, True)
+			Code = SMSreg.getCode(numResponse['tzid'])
+		
+		try:
+			self.client.sign_up(Code, fname, lname)
+
+		except telethon.errors.rpc_error_list.PhoneNumberOccupiedError:
+			self.log('This phone number is already occupied')
+			try:
+				self.client.sign_in(phone=self.phone, code=Code)
+			except telethon.errors.rpc_error_list.SessionPasswordNeededError:
+				SMSreg.finish(numResponse['tzid'])
+				self.__reg()
+		
+		'''
+		except Exception:
+			try:
+				self.client.sign_up(SMSreg.reviseCode(self.phone), fname, lname)
+			except Exception:
+				SMSreg.wrongCode(self.phone)
+		'''
+		SMSreg.finish(numResponse['tzid'])
+		self.log("Client is signed up")
 
 
 #public funcs:
 
-        def setOnline(self):
-                self.client(UpdateStatusRequest(False))
-                self.log('gone online')
+	def setOnline(self):
+		self.client(UpdateStatusRequest(False))
+		self.log('gone online')
 
-        def setOffline(self):
-                self.client(UpdateStatusRequest(True))
-                self.log('gone offline')
+	def setOffline(self):
+		self.client(UpdateStatusRequest(True))
+		self.log('gone offline')
 
-        def subscribe(self, channel):
-                self.client(JoinChannelRequest(self.client.get_entity(channel)))    
-                self.log('Joined ' + channel) 
+	def subscribe(self, channel):
+		self.client(JoinChannelRequest(self.client.get_entity(channel)))    
+		self.log('Joined ' + channel) 
 
-        def unsubscribe(self, channel):
-                self.client(LeaveChannelRequest(self.client.get_entity(channel)))
-                self.log('left '+channel)
+	def unsubscribe(self, channel):
+		self.client(LeaveChannelRequest(self.client.get_entity(channel)))
+		self.log('left '+channel)
 
 
 
